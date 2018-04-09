e7fbc18ac5ba764a654f1baf644a1cb269017c15
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2018-04-09 20:03:40 +0200

    Update blockchain.cpp

--- blockchain.cpp
+++ blockchain.cpp
@@ -1,4 +1,9 @@
 include "block.h"
+include "sha256.h"
+
+Blockchain::calculateHash(index, previousHash, timestamp, data) {
+    //return SHA256(index + previousHash + timestamp + data).toString();
+};
 
 int main() {
    cout << "Hello Blockchain";
