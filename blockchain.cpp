be781e83f411e1507d316c55d2ad7e6c98333065
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2018-04-09 21:30:10 +0200

    Update blockchain.cpp

--- blockchain.cpp
+++ blockchain.cpp
@@ -1,8 +1,9 @@
 include "block.h"
 include "sha256.h"
+include <time.h>
 
 std::string Blockchain::calculateHash(index, previousHash, timestamp, data) {
-    std::string common = std::to_string(index) + std::sName(&previousHash) + timestamp + data;
+    std::string common = std::to_string(index) + std::sName(&previousHash) + ctime(&timestamp) + data;
     return SHA256::sha256(common);
 };
 
